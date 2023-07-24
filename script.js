10508250a79b5d46aeb51f1d0314e4f55c13c235
“Laura <114937668+L-itslocked@users.noreply.github.com>
2023-07-24 14:10:33 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,29 @@
+let slider = tns({
+  container: ".my-slider",
+  slideBy: "1",
+  speed: 400,
+  nav: false,
+  autoplay: true,
+  controls: false,
+  arrowKey: true,
+  autoplayButtonOutput: false,
+  autoplayHoverPause: true,
+  responsive: {
+    1600: {
+      items: 4,
+      gutter: 10,
+    },
+    1024: {
+      items: 4,
+      gutter: 10,
+    },
+    768: {
+      items: 3,
+      gutter: 10,
+    },
+    480: {
+      items: 2,
+      gutter: 10,
+    },
+  },
+});
