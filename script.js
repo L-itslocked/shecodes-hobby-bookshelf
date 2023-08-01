91b09360d3cb6122124bd5c6be3d1378aa12b9dc
“Laura <114937668+L-itslocked@users.noreply.github.com>
2023-07-31 23:01:42 -0400

    Fix speed and gutter

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let slider = tns({
   container: ".my-slider",
   slideBy: "1",
-  speed: 400,
+  speed: 200,
   nav: false,
   autoplay: true,
   controls: false,
@@ -23,7 +23,7 @@ let slider = tns({
     },
     480: {
       items: 2,
-      gutter: 5,
+      gutter: 1,
     },
   },
 });
